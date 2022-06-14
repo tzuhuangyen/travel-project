be2d48573e0d6e8ec2f04699f64c2e3867e34c09
TZU HUANG YEN <912yan@gmail.com>
2022-06-14 16:17:30 +0200

    Update all.js

--- all.js
+++ all.js
@@ -2,7 +2,7 @@ let header = document.querySelector("header");
 let headerAnchor = document.querySelectorAll("header nav ul li a");
 
 window.addEventListener("scroll", () => {
-  if (window.pageYOFFset != 0) {
+  if (window.pageYOffset != 0) {
     header.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
     header.style.color = "white";
     headerAnchor.forEach((a) => {
